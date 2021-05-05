cb5f17a2b515e51a4bf5b7114d1478cecb7d4b59
shainarace <slrace@ncsu.edu>
2021-05-05 09:53:40 -0400

    Add files via upload

---
+++ _build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ev
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', clean=F)"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_book')"
+
+
